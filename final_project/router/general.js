6482b4f42c16c3a99e23196b2aa8ad8f500eea9c
AbdelrahmanAhmed605 <abdelrahman.ahmed605@hotmail.com>
2023-11-13 17:45:28 -0500

    Changed routes to be asynchronous using Promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,50 +32,90 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+// Simulating an asynchronous data retrieval with a Promise
+const getBooks = () => {
+    return new Promise((resolve) => {
+      // Simulating an asynchronous operation
+      setTimeout(() => {
+        resolve(books);
+      }, 1000);
+    });
+  };
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+    getBooks()
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((error) => {
+            console.error('Error retrieving books:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
     const isbn = req.params.isbn;
-    const isbnBook = books[isbn]
-    return res.status(200).json(isbnBook);
-});
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const booksByAuthor = []
-    for (let [isbn, book] of Object.entries(books)) {
-        if(book.author == author){
-            booksByAuthor.push({
-               $isbn: isbn,
-               title: book.title,
-               reviews: book.reviews 
-            })
+    getBooks()
+        .then((data) => {
+        const isbnBook = data[isbn];
+        if (isbnBook) {
+            res.status(200).json(isbnBook);
+        } else {
+            res.status(404).json({ error: 'Book not found' });
         }
-      }
+        })
+        .catch((error) => {
+            console.error('Error retrieving books:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
 
-    return res.status(200).json({booksByAuthor});
+// Get book details based on author
+public_users.get('/author/:author', (req, res) => {
+    const author = req.params.author;
+    getBooks()
+        .then((data) => {
+        const booksByAuthor = [];
+        for (let [isbn, book] of Object.entries(data)) {
+            if (book.author == author) {
+            booksByAuthor.push({
+                $isbn: isbn,
+                title: book.title,
+                reviews: book.reviews,
+            });
+            }
+        }
+        res.status(200).json({ booksByAuthor });
+        })
+        .catch((error) => {
+            console.error('Error retrieving books:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
-    const booksByTitle = []
-    for (let [isbn, book] of Object.entries(books)) {
-        if(book.title == title){
+    getBooks()
+        .then((data) => {
+        const booksByTitle = [];
+        for (let [isbn, book] of Object.entries(data)) {
+            if (book.title == title) {
             booksByTitle.push({
-               $isbn: isbn,
-               author: book.author,
-               reviews: book.reviews 
-            })
+                $isbn: isbn,
+                author: book.author,
+                reviews: book.reviews,
+            });
+            }
         }
-      }
-
-    return res.status(200).json({booksByTitle});
+        res.status(200).json({ booksByTitle });
+        })
+        .catch((error) => {
+            console.error('Error retrieving books:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
 
 //  Get book review
